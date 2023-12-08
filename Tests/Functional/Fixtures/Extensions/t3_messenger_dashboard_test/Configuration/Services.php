7bfa71be4ad8da89b8e4e9477d9620ac06cda5a9
Sebastian Schreiber <me@schreibersebastian.de>
2023-12-08 13:38:06 +0100

    TASK: Fix functional tests

--- Tests/Functional/Fixtures/Extensions/t3_messenger_dashboard_test/Configuration/Services.php
+++ Tests/Functional/Fixtures/Extensions/t3_messenger_dashboard_test/Configuration/Services.php
@@ -9,12 +9,8 @@ declare(strict_types=1);
  * LICENSE.txt file that was distributed with this source code.
  */
 
-use Ssch\T3Messenger\Tests\Functional\Fixtures\Extensions\t3_messenger_test\Classes\Controller\MessengerController;
-use Ssch\T3Messenger\Tests\Functional\Fixtures\Extensions\t3_messenger_test\Classes\EventListener\BeforeMailerSentMessageEventListener;
-use Ssch\T3Messenger\Tests\Functional\Fixtures\Extensions\t3_messenger_test\Classes\Handlers\MyMessengerHandler;
-use Ssch\T3Messenger\Tests\Functional\Fixtures\Extensions\t3_messenger_test\Classes\Service\MyService;
+use Ssch\T3MessengerDashboard\Tests\Functional\Fixtures\Extensions\t3_messenger_dashboard_test\Classes\Handlers\MyMessengerHandler;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use TYPO3\CMS\Core\Mail\Event\BeforeMailerSentMessageEvent;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -22,10 +18,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire()
         ->autoconfigure();
 
-    $services->set(MyService::class)->public();
     $services->set(MyMessengerHandler::class)->tag('messenger.message_handler');
-    $services->set(BeforeMailerSentMessageEventListener::class)->tag('event.listener', [
-        'event' => BeforeMailerSentMessageEvent::class,
-    ]);
-    $services->set(MessengerController::class);
 };
