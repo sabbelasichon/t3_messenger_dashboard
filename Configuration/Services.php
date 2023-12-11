805090959a7a77a2a4e1db683cb06432f33b5ccc
Sebastian Schreiber <me@schreibersebastian.de>
2023-12-11 23:12:33 +0100

    TASK: Make widget refreshable

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -44,6 +44,7 @@ return static function (ContainerConfigurator $containerConfigurator, ContainerB
         ->class(ListOfFailedMessagesWidget::class)
         ->arg('$dataProvider', service(FailedMessagesDataProvider::class))
         ->arg('$view', service('dashboard.views.widget'))
+        ->arg('$options', ['refreshAvailable' => true])
         ->tag(
             'dashboard.widget',
             [
