bbe6453f59e8982e9b9ff48d5fd272a4d80af4e1
Sebastian Schreiber <me@schreibersebastian.de>
2023-12-18 11:28:43 +0100

    TASK: Add background color for failed message link

--- Resources/Public/JavaScript/ScrollToFailedMessage.js
+++ Resources/Public/JavaScript/ScrollToFailedMessage.js
@@ -3,6 +3,7 @@ define(['jquery'], function ($) {
         var urlParams = new URLSearchParams(window.location.search);
         var $element = $(event.target).find('#failed-widget-' + urlParams.get('messageId'));
         if ($element.length) {
+            $element.css("background-color","#ffeeba");
             $element[0].scrollIntoView();
         }
     });
